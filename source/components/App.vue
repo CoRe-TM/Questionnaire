3bf0bd2a4b856b39087f0a58a5ef5fd89c00d4a6
Jan von der Assen <vonderassen@ifi.uzh.ch>
2022-02-11 15:27:52 +0100

    Generate skeleton

---
+++ source/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="front">
+        <h1>Hello {{ message }}!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App', data: () => ({
+        message: 'coretm-questionnaire'
+    })
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+    text-align: center;
+}
+</style>
